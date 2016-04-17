a56b19c47850fd9f47bc843f1186eea21ebb4f8b
ibininja <ibrahim.mokdad@gmail.com>
2016-04-17 11:41:44 +0300

    added code for video 14 on recording screen

---
+++ OpenCV3_Tutorials_14_Recording_Screen_with_OpenCV_and_PILLOW_using_Python.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+from PIL import ImageGrab
+
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+vid = cv2.VideoWriter('record.avi', fourcc, 8, (500,490))
+while(True):
+    img = ImageGrab.grab(bbox=(100, 10, 600, 500)) #x, y, w, h
+    img_np = np.array(img)
+    #frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+    vid.write(img_np)
+    cv2.imshow("frame", img_np)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break    
+
+vid.release()
+cv2.destroyAllWindows()
