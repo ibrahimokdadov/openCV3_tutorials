515b07193e9c666f572eb381e88dd94dc113f14f
Ibrahim MOKDAD <ibrahim.mokdad@gmail.com>
2016-09-23 12:20:13 +0300

    Update OpenCV3_Tutorials_3_Loading_Camera_in_OpenCV3_works_for_Videos .py
    added frame verification

--- OpenCV3_Tutorials_3_Loading_Camera_in_OpenCV3_works_for_Videos .py
+++ OpenCV3_Tutorials_3_Loading_Camera_in_OpenCV3_works_for_Videos .py
@@ -5,18 +5,22 @@ cam = cv2.VideoCapture(1)
 
 while(True):
     tf, frame = cam.read()
-    #print tf
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    upper_red = np.array([130,255,255])
-    lower_red = np.array([110,100,100])
-    mask = cv2.inRange(frame, lower_red, upper_red)
-    frame = cv2.bitwise_and(frame,frame, mask=mask)    
-    cv2.imshow('Single Frame', frame)
-    key = cv2.waitKey(1)
-    if key == 27: #esc key
+    if tf == True:
+        #print tf
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        upper_red = np.array([130,255,255])
+        lower_red = np.array([110,100,100])
+        mask = cv2.inRange(frame, lower_red, upper_red)
+        frame = cv2.bitwise_and(frame,frame, mask=mask)    
+        cv2.imshow('Single Frame', frame)
+        key = cv2.waitKey(1)
+        if key == 27: #esc key
+            break
+        elif key == ord('x'):
+            print "You have pressed the letter X"
+    else:
+        print "Could not read from camera...";
         break
-    elif key == ord('x'):
-        print "You have pressed the letter X"
 
 cam.release()
 cv2.destroyAllWindows()
